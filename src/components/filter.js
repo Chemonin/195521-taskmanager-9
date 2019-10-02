13c2e98e1f05dabb7e7f963834c9b2e7b1957e95
Chemonin <iachemonin@gmail.com>
2019-10-02 10:32:58 +0300

    замена всех компонентов в main.js классами

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,36 +1,21 @@
-export const createFilter = (dataList) => {
-  return `<section class="main__filter filter container">
-    ${Object.keys(dataList).map((key) => `<input
-      type="radio"
-      id="filter__${key}"
-      class="filter__input visually-hidden"
-      name="filter"
-      ${key === `all` ? `checked` : ``}
-      ${dataList[key] ? `` : `disabled`}
-    />
-    <label for="filter__${key}" class="filter__label">${key}<span class="filter__${key}-count">${dataList[key]}</span></label>`).join(``)}
-  </section>`;
-};
+import AbstractComponent from './abstract-component.js';
 
-// import AbstractComponent from './abstract-component.js';
-//
-// export default class Filter extends AbstractComponent {
-//   constructor(dataList) {
-//     super();
-//     this._dataList = dataList;
-//   }
-//
-//   getTemplate() {
-//     return `<section class="main__filter filter container">
-//       ${Object.keys(this._dataList).map((key) => `<input
-//         type="radio"
-//         id="filter__${key}"
-//         class="filter__input visually-hidden"
-//         name="filter"
-//         ${key === `all` ? `checked` : ``}
-//         ${this._dataList[key] ? `` : `disabled`}
-//       />
-//       <label for="filter__${key}" class="filter__label">${key}<span class="filter__${key}-count">${this._dataList[key]}</span></label>`).join(``)}
-//     </section>`;
-//   }
-// }
+export default class Filter extends AbstractComponent {
+  constructor(dataList) {
+    super();
+    this._dataList = dataList;
+  }
+  getTemplate() {
+    return `<section class="main__filter filter container">
+      ${Object.keys(this._dataList).map((key) => `<input
+        type="radio"
+        id="filter__${key}"
+        class="filter__input visually-hidden"
+        name="filter"
+        ${key === `all` ? `checked` : ``}
+        ${this._dataList[key] ? `` : `disabled`}
+      />
+      <label for="filter__${key}" class="filter__label">${key}<span class="filter__${key}-count">${this._dataList[key]}</span></label>`).join(``)}
+    </section>`;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,19 +1,24 @@
 import {createFilter} from './components/filter.js';
-import {createSearch} from './components/search.js';
-import {createMainMenuControl} from './components/main-menu.js';
 import {tasksData, filtersData} from './data.js';
 import BoardController from './controller/board-controller.js';
-
-const renderElement = function (container, position, item) {
-  container.insertAdjacentHTML(position, item);
-};
+import Statistics from './components/statistics.js';
+import MainMenu from './components/main-menu.js';
+import Search from './components/search.js';
+import Filter from './components/filter.js';
+import {Position, render} from './utils.js';
 
 const application = document.querySelector(`.main`);
-const mainMenu = document.querySelector(`.main__control`);
+const siteHeader = document.querySelector(`.main__control`);
 
-renderElement(mainMenu, `beforeend`, createMainMenuControl());
-renderElement(application, `beforeend`, createSearch());
-renderElement(application, `beforeend`, createFilter(filtersData));
+const statistics = new Statistics();
+const mainMenu = new MainMenu();
+const search = new Search();
+const filter = new Filter(filtersData);
+
+render(siteHeader, mainMenu.getElement(), Position.BEFOREEND);
+render(application, search.getElement(), Position.BEFOREEND);
+render(application, filter.getElement(), Position.BEFOREEND);
+render(application, statistics.getElement(), Position.BEFOREEND);
 
 const boardController = new BoardController(application, tasksData);
 boardController.init();
